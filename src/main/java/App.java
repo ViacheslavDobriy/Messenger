f86657e3a9a2735c787813fd467d5b6f4f077653
Slava Dobrov <dobrov.slava@yandex.ru>
2022-08-31 19:29:36 +0300

    This time I solved next stuff: 1) Created file for only one chat; 2) All messages between two people will be saved in file.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
+import java.io.IOException;
+
 /**
  * Точка входа здесь
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         UserInterface hello = new UserInterface();
         hello.hello();
     }
--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +15,14 @@ public class Chat {
     private Person person2;
     private List<Message> messagesData = new ArrayList<>();
 
-    public Chat(Person person1, Person person2) {
+    private Path path = Path.of("C:\\Users\\user\\Desktop\\GeekBrains\\Java\\Messenger\\chats.txt");
+
+    public Chat(Person person1, Person person2) throws IOException {
         this.person1=person1;
         this.person2=person2;
+        if (!Files.exists(path)) {
+            Files.createFile(Path.of(String.valueOf(path)));
+        }
     }
 
     public Person getPerson1() {
@@ -27,5 +35,14 @@ public class Chat {
 
     public List<Message> getMessagesData() {
         return messagesData;
+    }
+
+    public void writeMessageInFile() throws IOException {
+        String allMessages = "";
+        allMessages += Files.readString(path);
+        for (Message message: messagesData) {
+            allMessages += message.toString() + "\n";
+        }
+        Files.writeString(path, allMessages);
     }
 }
